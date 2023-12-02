19ec1658e0a5a6f89b50b5b59f5c836c8ae4498f
CodeNinjasSnellville <codeninjacrewmate@gmail.com>
2023-12-02 14:08:00 -0500

    aed

---
+++ Troy's meany bird/Assets/Scripts/GameController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    //Game over Canvas that is used for the game
+    [Header("Game Over UI Objects for displaying Game Over Screen")]
+    public GameObject gameOverCanvas;
+    [Header("Score UI Object for displaying Score")]
+    //Score Canvas that is used for the game
+    public GameObject scoreCanvas;
+    //Spawner object that is used for the game
+    [Header("Spawner Object for spawning objects in game")]
+    public GameObject spawner;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Speed for the game is at a playing state
+        Time.timeScale = 1;
+        //Score in visible
+        scoreCanvas.SetActive(true);
+        //Game Over UI is invisible
+        gameOverCanvas.SetActive(false);
+        //The spawner is shown in game
+        spawner.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
